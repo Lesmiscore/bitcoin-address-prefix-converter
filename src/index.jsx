c041e0f27a376f25fe56f1bb861c4e5f8a6c19a6
nao20010128nao <nao20010128@gmail.com>
2018-08-26 16:41:11 +0900

    変更

--- src/index.jsx
+++ src/index.jsx
@@ -6,29 +6,27 @@ import {
 
 import AddressConverterForm from "./convform.jsx";
 
-const App = () => {
-    return (
-        <div>
-            <Alert bsStyle="info">
-                <p>
-                    Without changing Hash part, this tools changes Prefix part of Bitcoin-like Base58 address.
-                    <br /> Bech32(SegWit), CashAddress(BCH), Zcash addresses are not supported.
-                    <br /> If it is Base58 address, ScriptSig address is also supported.
-                </p>
-                <p>
-                    BitcoinアドレスのようなBase58アドレスの、Hash部分を変更することなく、Prefixパートを変更します。
-                    <br /> Bech32(SegWit)、CashAddress(BCH)、Zcashアドレスには対応していませんのであしからず。
-                    <br /> Base58であればScriptSigアドレスにも対応しています。
-                </p>
-            </Alert>
-            <Alert bsStyle="danger">
-                <p>This tool won't require any private keys. If you prompted to type the key, IT'S SCAM.</p>
-                <p>このツールは秘密鍵を必要としません。入力を要求された場合、それは詐欺です。</p>
-            </Alert>
-            <AddressConverterForm />
-        </div>
-    )
-}
+const App = () => (
+    <div>
+        <Alert bsStyle="info">
+            <p>
+                Without changing Hash part, this tools changes Prefix part of Bitcoin-like Base58 address.
+                <br /> Bech32(SegWit), CashAddress(BCH), Zcash addresses are not supported.
+                <br /> If it is Base58 address, ScriptSig address is also supported.
+            </p>
+            <p>
+                BitcoinアドレスのようなBase58アドレスの、Hash部分を変更することなく、Prefixパートを変更します。
+                <br /> Bech32(SegWit)、CashAddress(BCH)、Zcashアドレスには対応していませんのであしからず。
+                <br /> Base58であればScriptSigアドレスにも対応しています。
+            </p>
+        </Alert>
+        <Alert bsStyle="danger">
+            <p>This tool won't require any private keys. If you prompted to type the key, IT'S SCAM.</p>
+            <p>このツールは秘密鍵を必要としません。入力を要求された場合、それは詐欺です。</p>
+        </Alert>
+        <AddressConverterForm />
+    </div>
+)
 
 function onLoad(func) {
     if (window.addEventListener)
